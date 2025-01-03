961c7b408f801226087857a747911e37dc6e7e2f
Aaron Kriegman <aaronkplus2@gmail.com>
2025-01-02 23:32:53 -0800

    notamessage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2655914950"
+    serviceWorkerVersion: "3211524152"
   }
 });
